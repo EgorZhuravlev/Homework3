ca9f1311dfb6d11913feab25e9e94ed5f30cd201
Egor Zhuravlev <zhuravlevegor@gmail.com>
2022-11-30 18:11:03 +0300

    Example1 added

---
+++ Example1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и 
+проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+if (!isParsed)
+{
+    Console.WriteLine("Координаты ввели не корректно");
+    return;
+}
+
+int originNumber = number;
+int rev = 0;
+
+while (number > 0)
+{
+    int dig = number % 10;
+    rev = rev * 10 + dig;
+    number = number / 10;
+}
+  if (originNumber == rev)
+    {
+        Console.WriteLine("Это палиндром!");
+    }
+    else
+    {
+        Console.WriteLine("Это не палиндром!");
+    }
